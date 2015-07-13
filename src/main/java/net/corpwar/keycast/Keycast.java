ff203d79b96835a3a80b58f4b7dbd96f7411d449
Ghost <ghostnr1@gmail.com>
2015-07-13 23:54:30 +0200

    Adding licenses in all src files. Free to use.

--- src/main/java/net/corpwar/keycast/Keycast.java
+++ src/main/java/net/corpwar/keycast/Keycast.java
@@ -1,3 +1,21 @@
+/**************************************************************************
+ * CorpNet
+ * Copyright (C) 2015 Daniel Ekedahl
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ **************************************************************************/
 package net.corpwar.keycast;
 
 import org.jnativehook.GlobalScreen;
@@ -17,7 +35,7 @@ import java.util.logging.Logger;
 
 /**
  * corpwar-keycast
- * Created by Ghost on 2015-07-12.
+ * Created by Daniel Ekedahl on 2015-07-12.
  */
 public class Keycast extends JFrame{
 
